982b6670af381b144f150132dc8a0b241158afe0
Tanaka Chingonzo <tchingonzo21@wooster.edu>
2018-04-05 14:12:27 -0400

    Running but buggy

---
+++ credentials.py
@@ -0,0 +1,12 @@
+# Credentials for your Twitter bot account
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = '4tW6x1B7zljgpVL4GlZE5sAA8'
+CONSUMER_SECRET = 'RbsEqmSjOFlzkKtstRBLqOtiSs99KA67MukWzeVvoDTFD20R8d'
+
+# Create a new Access Token
+ACCESS_TOKEN = '787025485217853446-Buo7Mi0GEhZZNdaYzZHlhr2e9o2oaVq'
+ACCESS_SECRET = '7gqtjzu2Fa5e5lZT08161qWsY5YFjFtJTfVF6hlkWgxlc'
